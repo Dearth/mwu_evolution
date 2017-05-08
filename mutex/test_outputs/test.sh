d6e2187bfdc1efaa961be0161fc56b026a648e9b
Dearth <Padraic Cashin>
2017-05-07 20:28:58 -0600

    Added more positive testcases for mutex

--- mutex/test_outputs/test.sh
+++ mutex/test_outputs/test.sh
@@ -21,7 +21,25 @@ do
 		else
 			exec diff inc2_out.txt inc2_out_$i.txt  > /dev/null 2>&1
 		fi;;
-		
+
+		p3) timeout 1 $1 $i
+		if [ $? -eq 124 ]; then
+			exit 1
+		else 
+			if [ `cat inc_out.txt | wc -l` -eq `cat inc2_out.txt | wc -l` ]; then 
+				exit 0
+			else 
+				exit 1
+			fi
+		fi;;
+
+		p4) timeout 1 $1 $i
+		if [ $? -eq 124 ]; then
+			exit 1
+		else 
+			exec diff inc2_out.txt output.txt > /dev/null 2>&1
+		fi;;
+
 		n1) timeout 1 $1 $i >& output.txt
 		if [ $? -eq 124 ]; then
 			exit 1
