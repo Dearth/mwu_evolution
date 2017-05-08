57230848f7ccfc64f9b739cedbf5b20d7d4afce3
Dearth <Padraic Cashin>
2017-05-07 19:42:32 -0600

    Added mutex test script v1

---
+++ mutex/test_outputs/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# $1 = EXE 
+# $2 = test name  
+# $3 = port 
+# exit 0 = success
+
+rm -rf inc_out.txt inc2_out.txt output.txt
+for i in {1..5}
+do
+	case $2 in
+		p1) timeout 1 $1 $i
+		if [ $? -eq 124 ]; then
+			exit 1
+		else
+			exec diff inc_out.txt inc_out_$i.txt  > /dev/null 2>&1
+		fi;;
+
+		p2) timeout 1 $1 $i
+		if [ $? -eq 124 ]; then
+			exit 1
+		else
+			exec diff inc2_out.txt inc2_out_$i.txt  > /dev/null 2>&1
+		fi;;
+		
+		n1) timeout 1 $1 $i >& output.txt
+		if [ $? -eq 124 ]; then
+			exit 1
+		else
+			exec diff output.txt output_$i.txt > /dev/null 2>&1
+		fi;;
+
+		*) exit 2;;        
+	esac
+done
+exit 1
